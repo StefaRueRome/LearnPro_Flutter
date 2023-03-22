4fb9a5fbdcf12534fef9f30cc96caceac4d072c9
Yurley Estefany Rueda Romero <misiontic.tutor31@uis.edu.co>
2023-03-21 20:07:46 -0500

    Eliminacion de la clase video_player_screen

--- lib/inicio.dart
+++ lib/inicio.dart
@@ -1,6 +1,5 @@
 import 'package:app_learn_pro/inicio_estructuras.dart';
 import 'package:app_learn_pro/inicio_paradigmas.dart';
-import 'package:app_learn_pro/video_player_screen.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:get/instance_manager.dart';
